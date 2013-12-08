6e6723fcb113555aaea959df638c85741883196b
SeyfiAslangeçinen <seyfiaslangecinen@gmail.com>
2013-12-08 20:47:03 +0200

    Single Page Application All pages Angular controllers and models Phalcon Micro Framework API

--- public/api/index.php
+++ public/api/index.php
@@ -8,28 +8,25 @@
 $loader = new \Phalcon\Loader();
 $loader->registerDirs(array(
     __DIR__ . '/app/model',
-    __DIR__ . '/app/library',
-    __DIR__ . '/app/routes',
+    __DIR__ . '/app/library'
 ))->register();
 $app = new Phalcon\Mvc\Micro();
 
 // Setup the database service
 // Comment out if you need
-/*
+
 $app['db'] = function() {
     return new Phalcon\Db\Adapter\Pdo\Mysql(array(
         "host" => "localhost",
-        "username" => "username",
-        "password" => "password",
-        "dbname" => "databasename"
+        "username" => "root",
+        "password" => "123456",
+        "dbname" => "acme"
     ));
 };
-*/
+
 
 $app->get('/', function () {
     echo "<h1>This is root</h1>";
 });
-$app->get('/hello', function () {
-    echo "<h1>Hello!</h1>";
-});
+include __DIR__ . "/app/routes/contact.php";
 $app->handle();
---
+++ public/assets/js/script.js
@@ -0,0 +1,47 @@
+/**
+ * Created by seyfi on 12/7/13.
+ */
+
+// create the module with a name and include route and controllers
+var app = angular.module('app', ['ngRoute', 'appControllers', 'ngResource']);
+
+// configure routes
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', { // default page
+            templateUrl: 'partials/home.html',
+            controller: 'mainController'
+        })
+        .when('/about', { // about page
+            templateUrl: 'partials/about.html',
+            controller: 'aboutController'
+        })
+        .when('/contact', { //contact page
+            templateUrl: 'partials/contact.html',
+            controller: 'contactController'
+        });
+});
+// create controllers
+var appControllers = angular.module('appControllers', []);
+
+app.controller('mainController', function ($scope) {
+    $scope.message = 'This is main page';
+});
+
+app.controller('aboutController', function ($scope) {
+    $scope.message = 'This is about page';
+});
+
+app.controller('contactController', ['$scope', '$resource', function ($scope, $resource) {
+
+    $scope.send = function () {
+        var data = {'name': $scope.name, 'email': $scope.email, 'message': $scope.message};
+        var contact = $resource('/api/contact', {}, {});
+        var response = contact.save({}, data);
+        $scope.name = "";
+        $scope.email = "";
+        $scope.message = "";
+
+
+    }
+}]);

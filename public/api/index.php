4dda772f7b3ec603edf5576ac6e3a7afc4abfcfe
SeyfiAslangeçinen <seyfiaslangecinen@gmail.com>
2013-12-08 23:52:49 +0200

    Make simpler api/index.php

--- public/api/index.php
+++ public/api/index.php
@@ -5,26 +5,8 @@
  *
  * Phalcon Micro Framework
  */
-$loader = new \Phalcon\Loader();
-$loader->registerDirs(array(
-    __DIR__ . '/app/model',
-    __DIR__ . '/app/library',
-    __DIR__ . '/app/routes',
-))->register();
-$app = new Phalcon\Mvc\Micro();
 
-// Setup the database service
-// Comment out if you need
-/*
-$app['db'] = function() {
-    return new Phalcon\Db\Adapter\Pdo\Mysql(array(
-        "host" => "localhost",
-        "username" => "username",
-        "password" => "password",
-        "dbname" => "databasename"
-    ));
-};
-*/
+$app = new Phalcon\Mvc\Micro();
 
 $app->get('/', function () {
     echo "<h1>This is root</h1>";
